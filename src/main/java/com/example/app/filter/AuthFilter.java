a7918bf1c2cbef66f8229a21d5068f506a2d11ea
Tomii <tomii@abc.com>
2023-09-11 15:05:28 +0900

    filterの追加

---
+++ src/main/java/com/example/app/filter/AuthFilter.java
@@ -0,0 +1,30 @@
+package com.example.app.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+//セッションにログインID を保持していない場合
+//トップページへ強制的に遷移させる処理を記述
+
+public class AuthFilter implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		if (session.getAttribute("loginId") == null) {
+			res.sendRedirect("/");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+}
