5ff625fe5e47d86a86add6ef7ec5e10763cd6573
Tomii <tomii@abc.com>
2023-09-11 15:21:35 +0900

    管理者ログインのdoGetの追記

--- src/main/java/com/example/app/controller/AdminLogin.java
+++ src/main/java/com/example/app/controller/AdminLogin.java
@@ -1,5 +1,15 @@
 package com.example.app.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/templates")
 public class AdminLogin {
 
+	@GetMapping
+	public String login() {
+		return "templates/login";
+	}
 }
