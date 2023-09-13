468cae4adb5cf651b98f96dc94e1e0009b4bab71
Tomii <tomii@abc.com>
2023-09-13 13:44:45 +0900

    domainの追加

---
+++ src/main/java/com/example/app/domain/Artist.java
@@ -0,0 +1,11 @@
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class Artist {
+
+	private Integer id;
+	private String name;
+	
+}
---
+++ src/main/java/com/example/app/domain/Live.java
@@ -0,0 +1,14 @@
+package com.example.app.domain;
+
+import lombok.Data;
+
+@Data
+public class Live {
+
+	
+	private Integer id;
+	private Integer created;
+	private String place;
+	private String iventName;
+	private Artist artist;
+}
