69f64d4defea36f2ff6a7ef8d00287d01b9c8885
Tomii <tomii@abc.com>
2023-09-13 10:48:27 +0900

    AdminからManagerに名称変更

--- src/main/java/com/example/app/domain/Manager.java
+++ src/main/java/com/example/app/domain/Manager.java
@@ -4,11 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class Admin {
+public class Manager {
 
 	private Integer id;
 	@NotBlank
 	private String loginId;
+	@NotBlank
 	private String loginPass;
 	private String name;
 }
